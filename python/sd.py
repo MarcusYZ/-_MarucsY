4e1615347494f45861dfb7c82b6eabd15cdbd962
dell <878187355@qq.com>
2019-05-15 11:33:30 +0800

    5.15

---
+++ python/sd.py
@@ -0,0 +1,53 @@
+
+
+# name = "ada lovelace"
+
+# print(name.title())
+
+# name = "Ada Lovelace"
+
+# print(name.upper())
+
+# print(name.lower()) 将字符串转化成小写再存储
+
+# 拼接字符串
+
+# first_name = "ada"
+
+# last_name = "lovelace"
+
+# full_name = first_name + " " + last_name
+
+# print(full_name)
+
+# 使用制表符或换行符添加空白
+
+# print("你好 \t 你好 \n 你好 ")
+
+# for 循环的使用
+# for l in [1,2,3,4]:
+#   print("数字",l)
+
+# 删除空白
+
+# message = "python  "
+
+# message = message.rstrip()
+
+# message
+
+# 使用str()避免类型错误
+
+# age = 23
+
+# message = "happy" + str(age) + "birthday"
+
+# print(message)
+
+list = ['1','2','3']
+
+print (list)
+
+
+
+
